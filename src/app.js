2791725740e5ef8cdf286b1b2a6417a40dd076a2
mayalenl <97466619+mayalenl@users.noreply.github.com>
2022-01-11 10:54:32 -0600

    add icon and alt text

--- src/app.js
+++ src/app.js
@@ -27,6 +27,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = response.data.main.temp;
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -34,8 +35,13 @@ function displayTemperature(response) {
   windElement.innerHTML = response.data.wind.speed;
   wind.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0], description);
 }
 let apiKey = "5492cef52b659b68fa6c6ab38ae9458b";
-let city = "Paris";
+let city = "Madrid";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
