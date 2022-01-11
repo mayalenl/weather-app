73e7c0f6621fa699d7d3050bedbcff6890e43f1c
mayalenl <97466619+mayalenl@users.noreply.github.com>
2022-01-10 19:27:09 -0600

    remove console log

--- src/app.js
+++ src/app.js
@@ -21,7 +21,6 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
